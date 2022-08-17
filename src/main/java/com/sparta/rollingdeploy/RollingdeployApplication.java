b247fb02a6e4e7e627b32b4f806dd762d8974ce6
Park-Seaweed <qkralsgur721@gmail.com>
2022-08-18 05:47:34 +0900

    젠킨스

---
+++ src/main/java/com/sparta/rollingdeploy/RollingdeployApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.rollingdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/rollingdeploy/RollingdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.rollingdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollingdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
